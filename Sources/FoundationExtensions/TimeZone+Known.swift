382ad001144724e769a7f21f59fa089cdbbd9390
abdullah-original <08.logins_jumbos@icloud.com>
2026-06-05 17:34:50 +0200

    Add known time zones

---
+++ Sources/FoundationExtensions/TimeZone+Known.swift
@@ -0,0 +1,104 @@
+import Foundation
+
+public extension TimeZone {
+    /// America/Halifax
+    static let adt = TimeZone(identifier: "ADT")
+    /// America/Juneau
+    static let akdt = TimeZone(identifier: "AKDT")
+    /// America/Juneau
+    static let akst = TimeZone(identifier: "AKST")
+    /// America/Argentina/Buenos_Aires
+    static let art = TimeZone(identifier: "ART")
+    /// America/Halifax
+    static let ast = TimeZone(identifier: "AST")
+    /// Asia/Dhaka
+    static let bdt = TimeZone(identifier: "BDT")
+    /// America/Sao_Paulo
+    static let brst = TimeZone(identifier: "BRST")
+    /// America/Sao_Paulo
+    static let brt = TimeZone(identifier: "BRT")
+    /// Europe/London
+    static let bst = TimeZone(identifier: "BST")
+    /// Africa/Harare
+    static let cat = TimeZone(identifier: "CAT")
+    /// America/Chicago
+    static let cdt = TimeZone(identifier: "CDT")
+    /// Europe/Paris
+    static let cest = TimeZone(identifier: "CEST")
+    /// Europe/Paris
+    static let cet = TimeZone(identifier: "CET")
+    /// America/Santiago
+    static let clst = TimeZone(identifier: "CLST")
+    /// America/Santiago
+    static let clt = TimeZone(identifier: "CLT")
+    /// America/Bogota
+    static let cot = TimeZone(identifier: "COT")
+    /// America/Chicago
+    static let cst = TimeZone(identifier: "CST")
+    /// Africa/Addis_Ababa
+    static let eat = TimeZone(identifier: "EAT")
+    /// America/New_York
+    static let edt = TimeZone(identifier: "EDT")
+    /// Europe/Athens
+    static let eest = TimeZone(identifier: "EEST")
+    /// Europe/Athens
+    static let eet = TimeZone(identifier: "EET")
+    /// America/New_York
+    static let est = TimeZone(identifier: "EST")
+    /// Asia/Dubai
+    static let gst = TimeZone(identifier: "GST")
+    /// Asia/Hong_Kong
+    static let hkt = TimeZone(identifier: "HKT")
+    /// Pacific/Honolulu
+    static let hst = TimeZone(identifier: "HST")
+    /// Asia/Bangkok
+    static let ict = TimeZone(identifier: "ICT")
+    /// Asia/Tehran
+    static let irst = TimeZone(identifier: "IRST")
+    /// Asia/Kolkata
+    static let ist = TimeZone(identifier: "IST")
+    /// Asia/Tokyo
+    static let jst = TimeZone(identifier: "JST")
+    /// Asia/Seoul
+    static let kst = TimeZone(identifier: "KST")
+    /// America/Denver
+    static let mdt = TimeZone(identifier: "MDT")
+    /// Europe/Moscow
+    static let msd = TimeZone(identifier: "MSD")
+    /// Europe/Moscow
+    static let msk = TimeZone(identifier: "MSK")
+    /// America/Phoenix
+    static let mst = TimeZone(identifier: "MST")
+    /// America/St_Johns
+    static let ndt = TimeZone(identifier: "NDT")
+    /// America/St_Johns
+    static let nst = TimeZone(identifier: "NST")
+    /// Pacific/Auckland
+    static let nzdt = TimeZone(identifier: "NZDT")
+    /// Pacific/Auckland
+    static let nzst = TimeZone(identifier: "NZST")
+    /// America/Los_Angeles
+    static let pdt = TimeZone(identifier: "PDT")
+    /// America/Lima
+    static let pet = TimeZone(identifier: "PET")
+    /// Asia/Manila
+    static let pht = TimeZone(identifier: "PHT")
+    /// Asia/Karachi
+    static let pkt = TimeZone(identifier: "PKT")
+    /// America/Los_Angeles
+    static let pst = TimeZone(identifier: "PST")
+    /// Asia/Singapore
+    static let sgt = TimeZone(identifier: "SGT")
+    /// Europe/Istanbul
+    static let trt = TimeZone(identifier: "TRT")
+    /// UTC
+    static let utc = TimeZone(identifier: "UTC")
+    /// Africa/Lagos
+    static let wat = TimeZone(identifier: "WAT")
+    /// Europe/Lisbon
+    static let west = TimeZone(identifier: "WEST")
+    /// Europe/Lisbon
+    static let wet = TimeZone(identifier: "WET")
+    /// Asia/Jakarta
+    static let wit = TimeZone(identifier: "WIT")
+}
